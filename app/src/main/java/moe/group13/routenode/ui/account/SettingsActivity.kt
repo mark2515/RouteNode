a4209a5340d00d3a147c4a91ad1d76d2be35a518
Nah <nah@Nahs-MacBook-Air.local>
2025-11-15 18:22:49 -0800

    Update UI for Account Page + adapt new structure

---
+++ app/src/main/java/moe/group13/routenode/ui/account/SettingsActivity.kt
@@ -0,0 +1,16 @@
+package moe.group13.routenode.ui.account
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import moe.group13.routenode.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+
+}
