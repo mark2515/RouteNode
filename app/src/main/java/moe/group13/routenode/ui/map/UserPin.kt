b8931ba43d9cec89c923cd4b861ca2b34c0771e4
edengler01 <erik@twistedware.com>
2025-11-21 12:54:49 -0800

    reverted favorites fragment, edited navbar to include map activity to test

--- app/src/main/java/moe/group13/routenode/ui/map/UserPin.kt
+++ app/src/main/java/moe/group13/routenode/ui/map/UserPin.kt
@@ -3,6 +3,7 @@ package moe.group13.routenode.ui.map
 import com.google.android.gms.maps.model.LatLng
 
 //class used to format pin
+//@TODO: Maybe be relevant to junh
 data class UserPin (
     //first destination name
     val destination: String,
